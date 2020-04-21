bca42660e6a2dbb9730d931bc1a4581ea724c83e
Sreekanth <62349142+sreekanth025@users.noreply.github.com>
2020-04-21 12:18:38 +0530

    Update binaryExponentiation.cpp

--- binaryExponentiation.cpp
+++ binaryExponentiation.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define int long long
 
 // (a^b) % m
+/*
 int power(int a, int b, int m)
 {
     if(b == 0)
@@ -14,6 +15,26 @@ int power(int a, int b, int m)
         return ((res%m)*(res%m)*(a%m))%m;
     else return ((res%m)*(res%m))%m;
 }
+*/
+
+
+int power(int x, unsigned int y, int p)  
+{  
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0; 
+  
+    while (y > 0)  
+    {  
+        if (y & 1)  
+            res = (res*x) % p;  
+  
+        y = y>>1; 
+        x = (x*x) % p;  
+    }  
+    return res;  
+}  
+
 
 int32_t main()
 {
